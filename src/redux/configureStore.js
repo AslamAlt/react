436c48ad0385106ee58d44789d35e9f2f5b46cce
AslamAlt <virusimproved@gmail.com>
2020-09-29 07:38:00 +0530

    Implemented combineReducers function

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,18 @@
-import { createStore } from "redux"
+import { combineReducers, createStore } from "redux"
+import { COMMENTS } from "../shared/comments";
+import { DISHES } from "../shared/dishes";
+import { LEADERS } from "../shared/leaders";
+import { PROMOTIONS } from "../shared/promotions";
 import { initialState, Reducer } from "./reducer"
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer,
-        initialState
+        combineReducers({
+            dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
+        })
     );
 
     return store;
