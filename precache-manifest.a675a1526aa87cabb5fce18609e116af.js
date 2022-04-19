aa894ab110c0621d33e66949518052c3fccc9a6d
AslamAlt <virusimproved@gmail.com>
2022-04-19 10:20:42 +0530

    Updates

---
+++ precache-manifest.a675a1526aa87cabb5fce18609e116af.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16763ac8686465059720db11078df4d4",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "af8863b8107757230104",
+    "url": "/react/static/css/2.3226332e.chunk.css"
+  },
+  {
+    "revision": "d8411dac29a1e7752fc0",
+    "url": "/react/static/css/main.60f8ff29.chunk.css"
+  },
+  {
+    "revision": "af8863b8107757230104",
+    "url": "/react/static/js/2.afccc672.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/react/static/js/2.afccc672.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8411dac29a1e7752fc0",
+    "url": "/react/static/js/main.cdd294eb.chunk.js"
+  },
+  {
+    "revision": "34ff4e8faaaee78fbc77",
+    "url": "/react/static/js/runtime-main.27356400.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
